979f2c04ed2ea2b0e6f16326860c878cbfe1d2fd
Flavio Magela <flavio.mrsantos@gmail.com>
2018-10-05 15:35:54 -0300

    Blindar a ListaNegociação para que não crie inserções indevidas - Classe Fornecedor

--- js/Models/ListaNegociacoes_Fornecedor.js
+++ js/Models/ListaNegociacoes_Fornecedor.js
@@ -2,18 +2,18 @@ class ListaNegociacoes_Fornecedor{
 
   constructor(){
 
-    this._negociacoes = []
+    this._negociacoes = [];
 
   }
 
   adiciona(negociacao){
 
-    this._negociacoes.push(negociacao);
+    this._negociacoes.push(negociacao); //metodo para ter acesso a lista de negocições
 
   }
   get negociacoes(){
 
-    return this._negociacoes;
+    return [].concat(this._negociacoes); //listar as negociações - return [] passa uma copia da _listaNegociacoes (blindar a lista)
 
   }
 
